accb523908acbf1bfbfdb41ce46e3a8ab8eaa308
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-05-31 18:59:17 +0100

    work with Environment variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('node:path');
 const app = express()
+require("dotenv").config();
 
 // set app properties (views folder path , template engine)
 app.set("views", path.join(__dirname, "views"));
